3783d8f42c38385e19b0520270734a964e98e73f
vin-sin12 <70302695+vin-sin12@users.noreply.github.com>
2021-10-20 23:22:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,46 +18,22 @@ function navAnimation(direction1, direction2) {
 }
 
 function toggleNav() {
-  // Toggle: Menu Bars Open/Closed
+  //Toggle: Menu Bars Open/Closed
   menuBars.classList.toggle('change')
 
-  //   Toggle: Menu Active
+  //Toggle: Menu Active
   overlay.classList.toggle('overlay-active')
   if (overlay.classList.contains('overlay-active')) {
-    // Animate In - Overlay
-    /*overlay.classList.add('overlay-slide-right')
-    overlay.classList.remove('overlay-slide-left')*/
+    //Animate In - Overlay
     overlay.classList.replace('overlay-slide-left', 'overlay-slide-right')
 
-    // *********Animate In - Nav Items*************
-    /*nav1.classList.add('slide-in-1')
-    nav1.classList.remove('slide-out-1')
-    nav2.classList.add('slide-in-2')
-    nav2.classList.remove('slide-out-2')
-    nav3.classList.add('slide-in-3')
-    nav3.classList.remove('slide-out-3')
-    nav4.classList.add('slide-in-4')
-    nav4.classList.remove('slide-out-4')
-    nav5.classList.add('slide-in-5')
-    nav5.classList.remove('slide-out-5')*/
+    //Animate In - Nav Items
     navAnimation('out', 'in')
   } else {
-    // Animate Out - Overlay
-    /*overlay.classList.add('overlay-slide-left')
-    overlay.classList.remove('overlay-slide-right')*/
+    //Animate Out - Overlay
     overlay.classList.replace('overlay-slide-right', 'overlay-slide-left')
 
-    //*******/ Animate Out - Nav Items**********
-    /*nav1.classList.add('slide-out-1')
-    nav1.classList.remove('slide-in-1')
-    nav2.classList.add('slide-out-2')
-    nav2.classList.remove('slide-in-2')
-    nav3.classList.add('slide-out-3')
-    nav3.classList.remove('slide-in-3')
-    nav4.classList.add('slide-out-4')
-    nav4.classList.remove('slide-in-4')
-    nav5.classList.add('slide-out-5')
-    nav5.classList.remove('slide-in-5')*/
+    //Animate Out - Nav Items
     navAnimation('in', 'out')
   }
 }
